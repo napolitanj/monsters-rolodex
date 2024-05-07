e1f14aa8ac1831e30a5947b2a72a1cb9f8dc5f71
JN <napolitanjoe@gmail.com>
2024-05-06 22:34:14 -0600

    Set button to change name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Yo whaddup.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: 'BROH', lastName: 'Brohameth'},
+      company: 'Jabrony inc.'
+    }
+  }
+
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+              Sup {this.state.name.firstName} {this.state.name.lastName}, why you working at {this.state.company}?
+          </p>
+          <button onClick={() => {
+            this.setState({name: { firstName: 'Brother', lastName: 'Bro'}})
+          }}
+          >
+          Change Name</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
