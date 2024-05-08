15e28c0832b2bef07c38a804574a24dce5aee04d
JN <napolitanjoe@gmail.com>
2024-05-08 14:33:57 -0600

    Add componentDidMount with API call

--- src/App.js
+++ src/App.js
@@ -8,25 +8,32 @@ class App extends Component {
     super();
 
     this.state = {
-    monsters: [
-      {
-        name: 'Tony'
-      },
-      {
-        name: 'Chrissy'
-      },
-      {
-        name: 'Paulie'
-      }
-    ]
+    monsters: []
   };
+}
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => 
+      this.setState(
+        () => {
+      return {monsters: users}
+    },
+    () => {
+      console.log(this.state);
+    }
+  )
+  );
 }
 
   render() {
     return <div className="App">
       {
         this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>
+          return <div key={monster.id}>
+          <h1>{monster.name}</h1>
+          </div>
         })}
     </div>;
   }
