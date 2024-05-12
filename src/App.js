10229877b13ce632928d4c034aac8bbd7856464d
JN <napolitanjoe@gmail.com>
2024-05-11 23:40:22 -0600

    Install google fonts and add title with styles

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ render() {
 
     return (
       <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           className='monsters-search-box'
           onChangeHandler={onSearchChange}
